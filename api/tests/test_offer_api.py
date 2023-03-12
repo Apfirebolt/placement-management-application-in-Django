935008ab9ff37408ab1c20ad6f9bc32a98bd8765
Amit Prafulla <amit@Amits-MacBook-Pro.local>
2023-03-13 00:11:05 +0530

    feat: test crud for offer

--- api/tests/test_offer_api.py
+++ api/tests/test_offer_api.py
@@ -85,4 +85,63 @@ class PrivateOfferApiTests(TestCase):
         self.assertEqual(offer.user_id, self.user)
         self.assertEqual(offer.notes, payload['notes'])
         self.assertEqual(offer.ctc, payload['ctc'])
+
     
+    def test_retrieve_offers(self):
+        """Test retrieving a list of offers."""
+        sample_company = create_company(user_id=self.user, name='Samsung')
+        create_offer(user_id=self.user, company_id=sample_company, notes='Note One', ctc='10 LPA', received_at=datetime.now())
+        create_offer(user_id=self.user, company_id=sample_company, notes='Note Two', ctc='22 LPA', received_at=datetime.now())
+
+        res = self.client.get(OFFERS_URL)
+
+        applications = Offer.objects.all().order_by('-id')
+        serializer = OfferSerializer(applications, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        # self.assertEqual(res.data, serializer.data)
+    
+
+    def test_get_offer_detail(self):
+        """Test get offer detail."""
+        company = create_company(user_id=self.user)
+        offer = create_offer(user_id=self.user, company_id=company, notes='Note Two', ctc='22 LPA', received_at=datetime.now())
+
+        url = detail_url(offer.id)
+        res = self.client.get(url)
+
+        serializer = OfferSerializer(offer)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    
+    def test_partial_offer_update(self):
+        """Test partial update of an offer."""
+        company = create_company(
+            user_id=self.user,
+            name='Sample company title',
+        )
+
+        offer = create_offer(user_id=self.user, company_id=company, notes='Note Two', ctc='22 LPA', received_at=datetime.now())
+
+        payload = {'ctc': '40 LPA'}
+        url = detail_url(offer.id)
+        # res = self.client.put(url, payload)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        offer.refresh_from_db()
+        self.assertEqual(offer.ctc, payload['ctc'])
+        self.assertEqual(offer.user_id, self.user)
+
+
+    def test_delete_offer(self):
+        """Test deleting an offer successful."""
+        company = create_company(user_id=self.user)
+
+        offer = create_offer(user_id=self.user, company_id=company, notes='Note Two', ctc='22 LPA', received_at=datetime.now())
+
+        url = detail_url(offer.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Offer.objects.filter(id=offer.id).exists())
