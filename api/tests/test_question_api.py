8c9a94ce087efbbf2e2bc02140068da4f26f0733
Amit Prafulla <amit@Amits-MacBook-Pro.local>
2023-03-12 19:19:46 +0530

    feat: full crud test cases for question

--- api/tests/test_question_api.py
+++ api/tests/test_question_api.py
@@ -17,7 +17,6 @@ from api.serializers import (
 )
 
 
-COMPANIES_URL = reverse('api:list-create-company')
 QUESTIONS_URL = reverse('api:list-create-question')
 
 
@@ -27,7 +26,7 @@ def detail_url(pk):
 
 
 def create_company(user_id, **params):
-    """Create and return a sample question."""
+    """Create and return a sample company."""
     defaults = {
         'name': 'Sample company name',
     }
@@ -35,6 +34,11 @@ def create_company(user_id, **params):
 
     company = Company.objects.create(user_id=user_id, **defaults)
     return company
+
+
+def create_question(user_id, **params):
+    question = Question.objects.create(user_id=user_id, **params)
+    return question
 
 
 def create_user(**params):
@@ -66,10 +70,10 @@ class PrivatequestionApiTests(TestCase):
 
     def test_create_question(self):
         """Test creating a Question."""
-        sampleCompany = create_company(user_id=self.user, name='Samsung')
+        sample_company = create_company(user_id=self.user, name='Samsung')
         payload = {
             'content': 'Sample question',
-            'company_id': sampleCompany.id
+            'company_id': sample_company.id
         }
         res = self.client.post(QUESTIONS_URL, payload)
 
@@ -78,7 +82,73 @@ class PrivatequestionApiTests(TestCase):
         self.assertEqual(question.user_id, self.user)
 
     
+    def test_retrieve_questions(self):
+        """Test retrieving a list of questions."""
+        sample_company = create_company(user_id=self.user, name='Samsung')
+        create_question(user_id=self.user, company_id=sample_company, content='Question One')
+        create_question(user_id=self.user, company_id=sample_company, content='Question Two')
+
+        res = self.client.get(QUESTIONS_URL)
+
+        questions = Question.objects.all().order_by('-id')
+        serializer = QuestionSerializer(questions, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        # self.assertEqual(res.data, serializer.data)
+
     
+    def test_get_question_detail(self):
+        """Test get question detail."""
+        company = create_company(user_id=self.user)
+        question = create_question(user_id=self.user, company_id=company, content='One Question')
+
+        url = detail_url(question.id)
+        res = self.client.get(url)
+
+        serializer = QuestionSerializer(question)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+    
+
+    def test_partial_question_update(self):
+        """Test partial update of a question."""
+        company = create_company(
+            user_id=self.user,
+            name='Sample company title',
+        )
+
+        question = create_question(
+            user_id=self.user,
+            company_id=company,
+            content='One Question'
+        )
+        payload = {'content': 'New question title'}
+        url = detail_url(question.id)
+        # res = self.client.put(url, payload)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        question.refresh_from_db()
+        self.assertEqual(question.content, payload['content'])
+        self.assertEqual(company.user_id, self.user)
+
+
+    def test_delete_question(self):
+        """Test deleting a question successful."""
+        company = create_company(user_id=self.user)
+
+        question = create_question(
+            user_id=self.user,
+            company_id=company,
+            content='One Question'
+        )
+
+        url = detail_url(question.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Question.objects.filter(id=question.id).exists())
+
+
 
 
 
